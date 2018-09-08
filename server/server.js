4e19f0996fc08c3df0019173d698fa1bcb0880c7
Kim Easton <keaston@Kims-MacBook-Pro.local>
2018-09-09 11:44:53 +1200

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
+const socketIO = require('socket.io');
 
 // add port environment variable for heroku to run
 const port = process.env.PORT || 3000;
@@ -9,15 +11,36 @@ console.log(__dirname + '/../public'); // old way
 console.log(publicPath); // new way
 
 var app = express();
+// create a http server, takes function (req, res) but so similar to express can simply
+// pass in app
+var server = http.createServer(app);
+// add this and now ready to accept new connections
+var io = socketIO(server);
 
-// serve a static resource
+// serve a static resource / middleware
 app.use(express.static(publicPath));
+
+// register an event listener
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+//  add event listener for disconnect from browser
+socket.on('disconnect', () => {
+    console.log('User was disconnected');
+    });
+});
+
+// change app.listen to server.listen
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
+
+
+
+
+
 
 // route handler
 // app.get('/', (req, res) => {
 //     res.send('<p> Welcome to my chat app! </p>');
 // });
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-});
