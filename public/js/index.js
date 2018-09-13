6a09f0ee9aba0bdedfb06c8044b442372da9343b
Kim Easton <keaston@Kims-MacBook-Pro.local>
2018-09-13 12:07:38 +1200

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -18,29 +18,50 @@ var socket = io();
             console.log('Disconnected from server');
     });
 
-// listen for new message
+// listen for new message  // // = commented out  // commented out for muctache
 socket.on('newMessage', function(message) {
-    // console.log('New message from server ', message);
     var formattedTime = moment(message.createdAt).format('h:mm a')
-    // var li = jQuery('<li></li>')
-    // li.text(`${message.from}: ${message.text}`);
-    $('#messages').append(`<li>${message.from} ${formattedTime} : ${message.text}</li>`);
-    // My code here clears the message text field
-    // $('[placeholder]').val(" ");
+    // add Mustache template
+    var template = jQuery('#message-template').html();
+    // pass in mustache template and all properties will be able to render
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
+    
+    // // console.log('New message from server ', message);
+    
+    // // var li = jQuery('<li></li>')
+    // // li.text(`${message.from}: ${message.text}`);
+    // $('#messages').append(`<li>${message.from} ${formattedTime} : ${message.text}</li>`);
+    // // My code here clears the message text field
+    // //$('[placeholder]').val(" ");
 });
 
     // listen for new location message
     socket.on('newLocationMessage', function(message) {
-         // Andrews method
-         var formattedTime = moment(message.createdAt).format('h:mm a')
-         var li = jQuery('<li></li>');
-         // target = blank opens a new tab rather than existing and kick out of chatroom
-         var a = jQuery('<a target="_blank">My Current Location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a')
+    // add Mustache template
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 
-         li.text(`${message.from} ${formattedTime} : `);
-         a.attr('href', message.url);
-         li.append(a);
-         jQuery('#messages').append(li);
+        //  // Andrews method
+        //  var formattedTime = moment(message.createdAt).format('h:mm a')
+        //  var li = jQuery('<li></li>');
+        //  // target = blank opens a new tab rather than existing and kick out of chatroom
+        //  var a = jQuery('<a target="_blank">My Current Location</a>');
+
+        //  li.text(`${message.from} ${formattedTime} : `);
+        //  a.attr('href', message.url);
+        //  li.append(a);
+        //  jQuery('#messages').append(li);
      });
 
 var messageTextBox = jQuery('[name=message]');
